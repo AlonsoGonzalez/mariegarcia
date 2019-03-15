0001f1cf944774f1111d8d5b83f1962804f67ed3
alonso <alomsoglez@gmail.com>
2019-03-14 22:04:12 -0600

    First commit

---
+++ src/App.js
@@ -0,0 +1,23 @@
+import React, { Component } from 'react';
+import "react-bootstrap/dist/react-bootstrap.min.js";
+import Navbar from 'react-bootstrap/Navbar';
+import Nav from 'react-bootstrap/Nav';
+import NavDropdown from 'react-bootstrap/NavDropdown';
+import Form from 'react-bootstrap/Form';
+import FormControl from 'react-bootstrap/FormControl';
+import Button from 'react-bootstrap/Button';
+import Carousel from 'react-bootstrap/Carousel';
+import './App.css';
+
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        Hola Marie Garcia
+      </div>
+    );
+  }
+}
+
+export default App;
